c542228366ade9c4ac1b18a4d0f37ec3d68c0ea1
Mohammed E H Shajib <shajib@Mohammeds-MacBook-Pro.local>
2018-03-06 16:12:30 -0500

    assignment4

--- MidProject/sketch.js
+++ MidProject/sketch.js
@@ -1,11 +1,16 @@
-function preload() {
+var background;
 
+
+
+
+function preload() {
+    background = loadImage("images/background.gif");
 }
 
 function setup() {
-
+    createCanvas(500, 500);
 }
 
 function draw() {
-
+    image(background, 0, 0);
 }
