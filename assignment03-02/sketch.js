811a27867a85670beb30336a76e57a7db30ba700
Mohammed E H Shajib <iamsajib1@gmail.com>
2018-05-09 16:58:23 -0400

    Add files via upload

---
+++ assignment03-02/sketch.js
@@ -0,0 +1,283 @@
+var carLeft, carRight, carUp, carDown;
+
+var theCar;
+
+var wallPic;
+
+var parking, borderW;
+
+var theWall, theWall2, theWall3, theWall4, theWall5, theWall6, theWall7, theWall8;
+
+var copL, copR, copC;
+
+var sound;
+
+var copX = 250;
+var copY = 250;
+
+var xDir = 3;
+var yDir = 3;
+
+var theWalls = [];
+
+function preload(){
+    carLeft = loadImage("images/carLeft.png");
+    carRight = loadImage("images/carRight.png");
+    carUp = loadImage("images/carUp.png");
+    carDown = loadImage("images/carDown.png");
+    copR = loadImage("images/policeR.png");
+    copL = loadImage("images/policeL.png");
+    wallPic = loadImage("images/wallOne.png");
+    parking = loadImage("images/parkingnew.png");
+    borderW = loadImage("images/borderW.png");
+    borderH = loadImage("images/borderH.png");
+    //background = loadImage("images/background.jpg");
+    sound = loadSound("sound/car.mp3");
+}
+
+function setup() {
+  createCanvas(1100, 700);
+    
+    imageMode(CENTER);
+    rectMode(CENTER);
+    
+    sound.play();
+
+
+    theCar = new Car(250, 250);
+
+    theWall = new Wall(180, 100);
+    theWall2 = new Wall(300, 530);
+    theWall3 = new Wall(90, 400);
+    theWall4 = new Wall(500, 330);
+    theWall5 = new Wall(700, 550);
+    theWall6 = new Wall(600, 100);
+    theWall7 = new Wall(900, 470);
+    theWall8 = new Wall(900, 200);
+
+    theWalls.push(theWall);
+    theWalls.push(theWall2);
+    theWalls.push(theWall3);
+    theWalls.push(theWall4);
+    theWalls.push(theWall5);
+    theWalls.push(theWall6);
+    theWalls.push(theWall7);
+    theWalls.push(theWall8);
+
+    copC = copL;
+}
+
+function draw() {
+
+    //image(background, 0, 0);
+    background(125, 125, 125);
+
+
+
+    theCar.move();
+    theCar.display();
+
+    theWall.display();
+    theWall2.display();
+    theWall3.display();
+    theWall4.display();
+    theWall5.display();
+    theWall6.display();
+    theWall7.display();
+    theWall8.display();
+
+//    image(parking, 1150, 7.5+height/2);
+    image(borderW, width/2, 0);
+    image(borderW, width/2, 700);
+    image(borderH, 0, height/2);
+    image(borderH, 1100, height/2);
+
+    //theWall.collide(theCar);
+
+
+//    if (copL.overlap(theCar)) {
+//        theCar.setSpeed(0, 0);
+//        //policeCar.setSpeed(0, 0);
+//        //alert("Game Over!");
+//
+//        textSize(90);
+//        text("Game Over", 400, 300);
+//        fill(0, 102, 153);
+//
+//    }
+
+
+
+
+    copX += xDir;
+    copY += yDir;
+
+
+  // did the ghost bounce?
+  if (copX > width-50) {
+    xDir *= -1;
+      copC= copR;
+  }
+  if (copX < 50) {
+    xDir *= -1;
+      copC = copL;
+
+  }
+
+  if (copY > height-50 || copY < 50) {
+    yDir *= -1;
+
+  }
+
+
+    image(copC, copX, copY);
+}
+
+
+
+
+class Wall{
+    constructor(startX, startY) {
+        this.x = startX;
+        this.y = startY;
+        this.graphic = wallPic;
+        this.left = this.x-42;
+        this.right = this.x+42;
+        this.top = this.y-50;
+        this.down = this.y+50;
+    }
+
+    display() {
+    image(this.graphic, this.x, this.y);
+    fill(255);
+    //ellipse(this.x,this.y,65);
+  }
+}
+
+class Car {
+
+  constructor(startX, startY) {
+
+    this.x = startX;
+    this.y = startY;
+
+    this.graphic = carRight;
+      
+    this.left = this.x-50;
+    this.right = this.x+50;
+    this.top = this.y-50;
+    this.down = this.y+50;
+  }
+
+  display() {
+
+    image(this.graphic, this.x, this.y);
+    fill(255,50);
+    //rect(this.x,this.y,100,100);
+  }
+
+
+  move() {
+
+    if (keyIsDown(65)) {
+        // see if there is a wall to the left
+        // assume there is not
+        var wallLeft = false;
+        for (var i = 0; i < theWalls.length; i++) {            
+            if(this.down > theWalls[i].top && this.top < theWalls[i].down) {
+                if (this.left-2.5 < theWalls[i].right && this.right > theWalls[i].right) {
+                    wallLeft = true;
+                }
+            }
+        }
+
+        // only move if there is no wall
+        if (wallLeft == false) {
+            this.x -= 2.5;
+        }
+        else {
+            // no move
+        }
+
+
+      this.graphic = carLeft;
+    }
+
+
+    if (keyIsDown(68)) {
+      var wallRight = false;
+ 
+        for (var i = 0; i < theWalls.length; i++) {            
+            if(this.down > theWalls[i].top && this.top < theWalls[i].down) {
+                if (this.right+2.5 > theWalls[i].left && this.left < theWalls[i].left) {
+                    wallRight = true;
+                }
+            }
+        }
+
+        // only move if there is no wall
+        if (wallRight == false) {
+            this.x += 2.5;
+        }
+        else {
+            // no move
+        }
+
+
+      this.graphic = carRight;
+    }
+      
+      
+    if (keyIsDown(87)) {
+
+        var wallUp = false;
+       
+          for (var i = 0; i < theWalls.length; i++) {            
+            if(this.right > theWalls[i].left && this.left < theWalls[i].right) {
+                if (this.top-2.5 < theWalls[i].down && this.down > theWalls[i].down) {
+                    wallUp = true;
+                }
+            }
+        }
+
+        // only move if there is no wall
+        if (wallUp == false) {
+            this.y -= 2.5;
+            }
+        else {
+            // no move
+        }
+
+      this.graphic = carUp;
+    }
+
+
+    if (keyIsDown(83)) {
+         var wallDown = false;
+      
+        for (var i = 0; i < theWalls.length; i++) {            
+            if(this.right > theWalls[i].left && this.left < theWalls[i].right) {
+                if (this.down+2.5 > theWalls[i].top && this.top < theWalls[i].top) {
+                    wallDown = true;
+                }
+            }
+        }
+
+        // only move if there is no wall
+        if (wallDown == false) {
+            this.y += 2.5;
+            }
+        else {
+            // no move
+        }
+      this.graphic = carDown;
+    }
+      
+    this.left = this.x-50;
+    this.right = this.x+50;
+    this.top = this.y-50;
+    this.down = this.y+50;
+
+  }
+
+}
